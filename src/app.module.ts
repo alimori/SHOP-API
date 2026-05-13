c1501439a344c997eeb205a559630ddd4fa56b1d
Ali Mortazavi <ali.mort@mtnirancell.ir>
2026-05-13 09:31:45 +0330

    Add app config and database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,54 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { CategoryModule } from './category/category.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'shop',
+    ConfigModule.forRoot({
+      isGlobal: true,
 
-      autoLoadEntities: true,
-      // synchronize: true in DEVELOPMENT, synchronize: false in PRODUCTION
-      synchronize: true,
+      load: [
+        appConfig,
+        databaseConfig,
+      ],
     }),
+
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: '127.0.0.1',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: '1234',
+    //   database: 'shop',
+
+    //   autoLoadEntities: true,
+    //   // synchronize: true in DEVELOPMENT, synchronize: false in PRODUCTION
+    //   synchronize: true,
+    // }),
+
+    TypeOrmModule.forRootAsync({
+
+      inject: [ConfigService],
+
+      useFactory: (
+        config: ConfigService,
+      ) => ({
+        type: 'postgres',
+        host: config.get<string>('database.host',),
+        port: config.get<number>('database.port',),
+        username: config.get<string>('database.username',),
+        password: config.get<string>('database.password',),
+        database: config.get<string>('database.name',),
+
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+
     ProductModule,
     OrderModule,
     CategoryModule,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,18 +16,21 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
+  const swaggerConfig  = new DocumentBuilder()
     .setTitle('Shop API')
     .setDescription('Product & Order API')
     .setVersion('1.0')
     .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
+  const document = SwaggerModule.createDocument(app, swaggerConfig );
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
 
-  // await app.listen(process.env.PORT ?? 3000);
+   const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('port') ?? 3000;
+
+  await app.listen(port);
+
+  //  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
