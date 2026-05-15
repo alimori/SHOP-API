ee4a8cf8870ef05617d0bc6b31c787a226738d54
Ali Mortazavi <ali.mort@mtnirancell.ir>
2026-05-15 16:48:19 +0330

    Add wrap response interceptor

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { ConfigModule } from '@nestjs/config';
 
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { WrapResponseInterceptor } from './interceptors/wrap-response.interceptor';
 
 @Global()
 @Module({
@@ -25,6 +26,12 @@ import { HttpExceptionFilter } from './filters/http-exception.filter';
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
+    },
+
+    // Global Interceptor
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: WrapResponseInterceptor,
     },
   ],
 
